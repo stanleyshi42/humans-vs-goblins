e8159fdb8f81b98a26673d3017b9d17e5096d1c7
stanleyshi42 <stanleyshi42@gmail.com>
2024-05-29 13:45:53 -0700

    added test for usePotion

--- src/tests/PlayerTest.java
+++ src/tests/PlayerTest.java
@@ -19,6 +19,28 @@ public class PlayerTest {
 
 		assertEquals(player.equipment.get("weapon").getId(), ItemID.IRON_SWORD);
 		assertFalse(player.inventory.contains(weapon));
-		
+
+	}
+
+	@Test
+	public void testUsePotion() {
+		Player player = new Player();
+		player.curHp = 0;
+
+		for (Item i : player.inventory) {
+			if (i.getId() == ItemID.SMALL_POTION) {
+				player.usePotion((Potion) i);
+				assertEquals(3, player.curHp);
+				assertFalse(player.inventory.contains(i));
+				break;
+			}
+		}
+
+		player.inventory.add(new Potion(ItemID.MEDIUM_POTION));
+		player.curHp = 0;
+		player.usePotion(ItemID.MEDIUM_POTION);
+
+		assertEquals(5, player.curHp);
+
 	}
 }
