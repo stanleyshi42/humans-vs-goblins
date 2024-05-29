aaa38563fe0886fd173a7af2a77b5e1bea043582
stanleyshi42 <stanleyshi42@gmail.com>
2024-05-29 07:09:02 -0700

    started game loop logic

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -4,6 +4,14 @@ public class Player extends Entity {
 	int x, y;
 	int hp, attack, defense;
 
+	public Player() {
+		this.x = 0;
+		this.y = 0;
+		this.hp = 10;
+		this.attack = 5;
+		this.defense = 5;
+	}
+
 	@Override
 	public void move() {
 		// TODO Auto-generated method stub
---
+++ src/humansVsGoblins/GameLoop.java
@@ -0,0 +1,12 @@
+package humansVsGoblins;
+
+import entities.Player;
+
+public class GameLoop {
+	Player player = new Player();
+
+	public static void main(String[] args) {
+
+	}
+
+}
