7afda8ff4e6e097956fef551eb573563892fb5aa
Hy <hyhuynh222@gmail.com>
2024-05-29 08:13:14 -0700

    Added Movement

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -12,6 +12,22 @@ public class Player extends Entity {
 		this.defense = 5;
 	}
 
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x += x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y += y;
+	}
+
 	@Override
 	public void move() {
 		// TODO Auto-generated method stub
