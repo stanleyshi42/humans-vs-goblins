4de0b18c131959c6df0900227e8477602194c6a9
Hy <hyhuynh222@gmail.com>
2024-05-29 13:21:29 -0700

    Test

--- src/humansVsGoblins/GamePanel.java
+++ src/humansVsGoblins/GamePanel.java
@@ -35,7 +35,6 @@ public class GamePanel extends JPanel implements Runnable {
     
     ArrayList<Tile> mapTiles = new ArrayList<>();
     Thread gameThread;
-    KeyHandler keyHandler = new KeyHandler();
     TileResource tileResource = new TileResource(this);
 
     JPanel selectedPanel = null;
@@ -60,34 +59,16 @@ public class GamePanel extends JPanel implements Runnable {
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
         this.setVisible(true);
-        this.addKeyListener(keyHandler);
+        //this.addKeyListener(keyHandler);
         this.setFocusable(true);
-        addMouseListener(new MouseAdapter() {
-            @Override
-            public void mousePressed(MouseEvent e) {
-                JPanel panel = (JPanel) getComponentAt(e.getPoint());
-                if (panel == null || panel == GamePanel.this) {
-                    return;
-                }
-//                if (selectedPanel != null) {
-//                    player.setX(e.getX());
-//                    player.setY(e.getY());
-//                }
-                selectedPanel = panel;
-//                selectedPanel.add(new JLabel(selectedPanel.getName()));
-//                selectedPanel.revalidate();
-                player.setY(Integer.parseInt(selectedPanel.getName().split(" ")[0]) * 48);
-                player.setX(Integer.parseInt(selectedPanel.getName().split(" ")[1]) * 48);
-
-            }
-        });
+        this.addMouseListener(new KeyHandler(this,player));
 
     }
 
 
     @Override
     protected void paintComponent(Graphics g) {
-        
+
         Graphics2D g2 = (Graphics2D)g;
         tileResource.draw(g2);
         g2.setColor(Color.BLUE);
@@ -118,15 +99,15 @@ public class GamePanel extends JPanel implements Runnable {
     }
 
     public void update(){
-        if (keyHandler.upPressed){
-            player.setY(-5);
-        } else if (keyHandler.downPressed){
-            player.setY(5);
-        } else if (keyHandler.rightPressed){
-            player.setX(5);
-        } else if (keyHandler.leftPressed){
-            player.setX(-5);
-        }
+//        if (keyHandler.upPressed){
+//            player.setY(-5);
+//        } else if (keyHandler.downPressed){
+//            player.setY(5);
+//        } else if (keyHandler.rightPressed){
+//            player.setX(5);
+//        } else if (keyHandler.leftPressed){
+//            player.setX(-5);
+//        }
     }
 
     public Player getPlayer() {
--- src/humansVsGoblins/KeyHandler.java
+++ src/humansVsGoblins/KeyHandler.java
@@ -1,55 +1,29 @@
 package humansVsGoblins;
 
-import java.awt.event.KeyListener;
-import java.awt.event.KeyEvent;
+import entities.Player;
 
-public class KeyHandler implements KeyListener {
-    public boolean upPressed, leftPressed, downPressed, rightPressed;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JPanel;
 
-    @Override
-    public void keyTyped(KeyEvent e) {
+public class KeyHandler extends MouseAdapter {
+    private JPanel selectedPanel;
+    private final JPanel gamePanel;
+    private final Player player;
+
+    public KeyHandler(JPanel gamePanel, Player player) {
+        this.gamePanel = gamePanel;
+        this.player = player;
     }
 
     @Override
-    public void keyPressed(KeyEvent e) {
-        switch(e.getKeyCode()) {
-            case KeyEvent.VK_LEFT:
-                leftPressed = true;
-                break;
-
-            case KeyEvent.VK_RIGHT:
-                rightPressed = true;
-                break;
-
-            case KeyEvent.VK_UP:
-                upPressed = true;
-                break;
-
-            case KeyEvent.VK_DOWN:
-                downPressed = true;
-                break;
+    public void mousePressed(MouseEvent e) {
+        JPanel panel = (JPanel) gamePanel.getComponentAt(e.getPoint());
+        if (panel == null || panel == gamePanel) {
+            return;
         }
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-        switch(e.getKeyCode()) {
-            case KeyEvent.VK_LEFT:
-                leftPressed = false;
-                break;
-
-            case KeyEvent.VK_RIGHT:
-                rightPressed = false;
-                break;
-
-            case KeyEvent.VK_UP:
-                upPressed = false;
-                break;
-
-            case KeyEvent.VK_DOWN:
-                downPressed = false;
-                break;
-        }
-
+        selectedPanel = panel;
+        player.setY(Integer.parseInt(selectedPanel.getName().split(" ")[0]) * 48);
+        player.setX(Integer.parseInt(selectedPanel.getName().split(" ")[1]) * 48);
     }
 }
