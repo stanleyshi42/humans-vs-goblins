e947e7a65c114d90ca9de64845bb5c2919cd08ac
Hy <hyhuynh222@gmail.com>
2024-05-29 06:42:36 -0700

    Added GameFrame

---
+++ src/humansVsGoblins/GameFrame.java
@@ -0,0 +1,15 @@
+package humansVsGoblins;
+
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+
+	GameFrame() {
+		this.setTitle("Human Vs Goblin");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+	}
+
+}
