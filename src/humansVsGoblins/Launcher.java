1a11f7eaf2ceb42d7931bda4e5524ea94feedae0
Anthony Alvarez <101856161+PythonicRhythm@users.noreply.github.com>
2024-05-30 15:40:15 -0500

    Refactored KeyHandler

--- src/humansVsGoblins/Launcher.java
+++ src/humansVsGoblins/Launcher.java
@@ -3,9 +3,9 @@ package humansVsGoblins;
 public class Launcher {
     
     public static void main(String[] args) {
-        //GameFrame window = new GameFrame();
-        GamePanel panel = new GamePanel();
-        InventoryPanel invPanel = new InventoryPanel(panel, panel.getPlayer());
+        GameFrame window = new GameFrame();
+        //GamePanel panel = new GamePanel();
+        //InventoryPanel invPanel = new InventoryPanel(panel, panel.getPlayer());
         //window.add(invPanel);
 
         //window.pack();
