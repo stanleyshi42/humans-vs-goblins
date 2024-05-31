eb2945a96b31be64bd2520f6deb2275653e148d2
stanleyshi42 <stanleyshi42@gmail.com>
2024-05-31 10:35:40 -0700

    added sprite

--- src/items/Key.java
+++ src/items/Key.java
@@ -1,8 +1,11 @@
 package items;
 
+import javax.swing.ImageIcon;
+
 public class Key extends Item {
 	public Key(ItemID itemId) {
 		super("Key", itemId, null);
+		this.setSprite(new ImageIcon("Resources/key.png"));
 
 	}
 
